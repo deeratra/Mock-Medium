003c46850eeea01b2b662dc2dca2987f00e57956
Deepankar Ratra <drattra@gmail.com>
2024-03-10 10:22:35 +0100

    Update code

--- frontend/src/components/PostCard.tsx
+++ frontend/src/components/PostCard.tsx
@@ -37,7 +37,7 @@ export const PostCard = ({ authorName, title, content, publishedDate, id }: Post
 
 
 export function Avatar({ name = 'Test', size = 'big' }: { name?: string, size?: string }) {
-    return <div className={`relative inline-flex items-center justify-center w-9 h-9 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600`}>
+    return <div className={`relative inline-flex items-center justify-center ${size} w-9 h-9 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600`}>
         <span className="font-medium text-gray-600 dark:text-gray-300">{name ? name[0] : "H"}</span>
     </div>
 }
